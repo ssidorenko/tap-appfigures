c245650a15e5bbc05271f04f80254b7c4c33092c
Jesue Junior <jesuesousa@gmail.com>
2022-02-06 21:10:40 -0300

    Adding reports to get 30 days batch size

--- tap_appfigures/streams/revenue.py
+++ tap_appfigures/streams/revenue.py
@@ -3,5 +3,5 @@ from tap_appfigures.streams.base import AppFiguresBase
 
 class RevenueStream(AppFiguresBase):
     STREAM_NAME = 'revenue'
-    URI = '/reports/revenue?group_by=products,dates&start_date={}&granularity=daily'
+    URI = '/reports/revenue?group_by=products,dates&start_date={}&end_date={}&granularity=daily'
     KEY_PROPERTIES = ['product_id', 'date']
