918ba3f30e55b801aee65a4cfbeb213c968a527f
Jesue Junior <jesuesousa@gmail.com>
2022-02-26 13:27:16 -0300

    Adding group by countries in sales and revenue

--- tap_appfigures/streams/revenue.py
+++ tap_appfigures/streams/revenue.py
@@ -3,5 +3,6 @@ from tap_appfigures.streams.base import AppFiguresBase
 
 class RevenueStream(AppFiguresBase):
     STREAM_NAME = 'revenue'
-    URI = '/reports/revenue?group_by=products,dates&start_date={}&end_date={}&granularity=daily'
-    KEY_PROPERTIES = ['product_id', 'date']
+    URI = '/reports/revenue?group_by=products,dates,country&start_date={}&end_date={}&granularity=daily'
+    KEY_PROPERTIES = ["country", 'product_id', 'date']
+    RESPONSE_LEVELS = 3
--- tap_appfigures/streams/sales.py
+++ tap_appfigures/streams/sales.py
@@ -3,5 +3,6 @@ from tap_appfigures.streams.base import AppFiguresBase
 
 class SalesStream(AppFiguresBase):
     STREAM_NAME = 'sales'
-    URI = '/reports/sales/?group_by=products,dates&start_date={}&end_date={}&granularity=daily'
-    KEY_PROPERTIES = ['product_id', 'date']
+    URI = '/reports/sales/?group_by=products,dates,countries&start_date={}&end_date={}&granularity=daily'
+    KEY_PROPERTIES = ["country",'product_id', 'date']
+    RESPONSE_LEVELS = 3
