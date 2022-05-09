47f9d7e1cb909850823cfb827fc15c0ec6f8d85d
Jesue Junior <jesuesousa@gmail.com>
2022-05-08 22:02:06 -0300

    Adding a fix when theresnt an inapp for ranks

--- tap_appfigures/streams/ranks.py
+++ tap_appfigures/streams/ranks.py
@@ -1,11 +1,14 @@
+import re
 from datetime import date, timedelta
 
 import singer
 
 from tap_appfigures.streams.base import AppFiguresBase
 from tap_appfigures.utils import str_to_date, date_to_str, strings_to_floats
+from tap_appfigures.utils import RequestError
 
 LOGGER = singer.get_logger()
+
 
 class RanksStream(AppFiguresBase):
     STREAM_NAME = 'ranks'
@@ -14,7 +17,7 @@ class RanksStream(AppFiguresBase):
     def do_sync(self):
         start_date = str_to_date(self.bookmark_date)
         new_bookmark_date = start_date
-        
+
         # README: BEGIN -> When it has more than 15 app_id, it has to be splited
         products = []
         final_list = lambda _list, x: [_list[i:i+x] for i in range(0, len(_list), x)]
@@ -30,12 +33,15 @@ class RanksStream(AppFiguresBase):
                     start_date.strftime('%Y-%m-%d'),
                     end_date.strftime('%Y-%m-%d')
                 )
-
                 data = self.client.make_request(uri).json()
-                import re
-                if data.get("status") == 400:
+                while data.get("status") == 400:
                     _id = re.findall('\d+', data['message'])
-                    LOGGER.warning(f"This app ID doesnt exist in ranks: {_id}")
+                    LOGGER.warning(f"This app ID doesnt exist in inapp for ranks: {_id}, removing and trying again")
+                    uri = re.sub(f",{_id[0]}|{_id[0]},", "", uri)
+                    if "," not in uri:
+                        break
+                    data = self.client.make_request(uri).json()
+                if data.get("status") == 400:
                     continue
                 rank_dates = data['dates']
                 rank_data = data['data']
